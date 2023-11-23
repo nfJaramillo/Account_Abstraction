3556141836a085fbabbc1655689005fc2468c9d4
Nicolás F. Jaramillo <nf.jaramillo@uniandes.edu.co>
2023-11-23 09:37:40 -0500

    Base commit
    Base code from Blockchain4Bintec

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import eslint from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/Blockchain4Bintec/',
+  plugins: [react(), eslint()],
+  define: {
+    global: {},
+    "process.env": {}
+  },
+  server:{
+    port: 3000
+  }
+})
